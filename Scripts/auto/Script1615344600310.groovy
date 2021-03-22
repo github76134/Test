da10484ac17c86e3754f97ec64d0299344af7eae
Le Hieu <tronghieu07th@gmail.com>
2021-03-22 16:17:00 +0700

    cmt

--- Scripts/auto/Script1615344600310.groovy
+++ Scripts/auto/Script1615344600310.groovy
@@ -16,6 +16,4 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-//test sai rồi
-
-//Như vậy mới đúng nè
+// Tạo ra nhánh mới rồi nha
