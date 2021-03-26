b55cb497e1890a379c6a8438ce008e7a5717312a
Le Hieu <tronghieu07th@gmail.com>
2021-03-26 09:17:02 +0700

    cmt

--- Test Suites/time_2.ts
+++ Test Suites/time_2.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>abc_2</name>
+   <name>time_2</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
