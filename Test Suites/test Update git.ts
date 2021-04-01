68f1694ce567dbafc5e7be9c656b3bfccd0ea02f
Le Hieu <tronghieu07th@gmail.com>
2021-04-01 13:46:21 +0700

    update git nhanh test

---
+++ Test Suites/test Update git.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test Update git</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>387fb98a-91ff-4f24-bad1-45ca9bb66927</testSuiteGuid>
+</TestSuiteEntity>
